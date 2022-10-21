075e0d43827a8fcba994d2bd0d1c7979e64fc721
Mostafa ElGendi <mielguindi@gmail.com>
2022-10-21 12:16:29 +0200

    add docs

--- lib/hola_mostafa.rb
+++ lib/hola_mostafa.rb
@@ -1,6 +1,17 @@
 require 'hola_mostafa/translator'
 
+# The main HolaMostafa driver
 module HolaMostafa
+  # Say hi to the world of Ruby!
+  # in multiple languages!
+  #
+  # @example
+  #  >> HolaMostafa.hi('english')
+  #  => 'hello'
+  #
+  # @param
+  #   language: (String)
+
   def self.hi(language = 'arabic')
     translator = Translator.new(language)
     translator.hi
